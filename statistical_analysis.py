3b462e4e0af17d089bb01fbe467e42ae9393e3a6
tylerharrold <tylerharrold@gmail.com>
2019-02-19 12:24:57 -0500

    Added basic statistical analysis methods

--- statistical_analysis.py
+++ statistical_analysis.py
@@ -1,4 +1,41 @@
 import json
+import math
+from pathlib import Path
 
-def return mean_auc(jsonfile):
-    pass
+def get_generation_mean(json_data):
+    data = json_data['children']
+    len_data = len(data)
+    mean = 0
+    for d in data:
+        mean = mean + d['area_under_curve']
+    mean = mean / len_data
+    return mean
+
+def get_generation_variance(json_data , sample_mean):
+    sigma = 0
+    data = json_data['children']
+    n = len(data)
+    for d in data:
+        sigma = sigma + (d['area_under_curve'] - sample_mean)**2
+    variance = sigma / (n - 1)
+    return variance
+
+def get_generation_stddev(gen_variance):
+    return math.sqrt(gen_variance)
+
+
+def print_stats(json):
+    mean = get_generation_mean(json)
+    variance = get_generation_variance(json, mean)
+    stddev = get_generation_stddev(variance)
+    print('mean: ' , mean)
+    print('var: ' , variance)
+    print('stddev: ' , stddev)
+
+
+
+if __name__ == "__main__":
+    report = Path.cwd() / "Reports" / "layer_swap_40_report" / "generation_30_report" / "gen_report.json"
+    with open(str(report)) as jsonfile:
+        data = json.load(jsonfile)
+    print_stats(data)
