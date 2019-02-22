76fedafb2401e5a65df814ec6e66bb4dd7b66cd5
tylerharrold <tylerharrold@gmail.com>
2019-02-21 22:24:48 -0500

    Began a refactor for reporting tools

--- reporting.py
+++ reporting.py
@@ -4,11 +4,25 @@
 import json
 from pathlib import Path
 from Genetics_Refactored import Genetics
+import tools
 
 
 # returns a dict of data for an individual model (i.e. a child)
-def get_individual_model_report(dna_string , losses_list):
-    pass
+def get_individual_model_report(name , dna_string , losses_list, eval_type = 'auc' , legacy_dna=False):
+    report = {}
+    perplexity, shape = Genetics.decode_dna(dna_string , legacy_dna=legacy_dna)
+    report['dna'] = dna_string
+    report['perplexity'] = perplexity
+    report['shape'] = shape
+    report['name'] = name
+    if eval_type is 'auc':
+        eval = tools.get_area_under_curve(losses_list)
+    elif eval_type is 'half_auc':
+        eval = tools.get_area_under_half_curve(losses_list)
+    else:
+        print("critical error")
+    report[eval_type] = eval
+    return report
 
 # returns a dict of data for an entire training generation
 def get_generation_report():
